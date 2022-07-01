04c82df5385da4930af1883170f8c4559a68455c
freddypimpns <freddypimpns@gmail.com>
2022-07-01 11:10:58 -0400

    condicion para verificar si han llegado imagenes

--- app/Http/Controllers/puntosController.php
+++ app/Http/Controllers/puntosController.php
@@ -42,8 +42,10 @@ class puntosController extends Controller
       $NewPunto->Latitud = $request->input('Latitud');
       $NewPunto->Type = $request->input('Type');
       $NewPunto->User = $request->input('User');
-      $paths = $request->image->store('img/puntos/');
-      $NewPunto->imagen = $paths;
+      if($request->image){
+        $paths = $request->image->store('img/puntos/');
+        $NewPunto->imagen = $paths;
+      }
       $NewPunto->save();
       
     }
