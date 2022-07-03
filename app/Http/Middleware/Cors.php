89dfe4edff98436ce40503d11702d5ac2ec75c1f
freddypimpns <freddypimpns@gmail.com>
2022-07-03 14:10:21 -0400

    muchos intentos despues

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header("Access-Control-Allow-Origin: http://warm-mountain-66927.herokuapp.com/public/api/puntos")
+        ->header("Access-Control-Allow-Origin","*")
         ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-        ->header("Access-Control-Allow-Headers", "Content-Type");
+        ->header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 }
